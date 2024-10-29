1fe05fad7f1dcdb74cfb3b58e1f9f4a6cab4bf5b
Matheus Saturnino <mattsat3@gmail.com>
2024-10-29 01:07:20 -0400

    implementando o resultado, bloqueio dos botões e remoção do css

--- src/javascritp/script.js
+++ src/javascritp/script.js
@@ -6,8 +6,8 @@ const maybeButton = document.getElementById('maybe');
 const maybeValue = document.getElementById('value-maybe');
 const abortButton = document.getElementById('abort');
 const correctButton = document.getElementById('correct');
-var secretword = document.getElementById('secretword');
-
+const restartButton = document.getElementById('restart');
+const secretword = document.getElementById('secretword');
 
 let countyes = 0;
 let countno = 0;
@@ -40,17 +40,53 @@ maybeButton.addEventListener('click',()=> {
     updatemaybeValue();
 });
 
-abortButton.addEventListener('click',()=> {
-    countyes = 0;
-    countno = 0;
-    countmaybe = 0;
-    updatemaybeValue();
-    updatenoValue();
-    updateyesValue();
-    secretword.type = "password";
-    secretword.value = ""; 
+correctButton.addEventListener('click',()=>{ 
+    secretword.type = "text"; //Revela a palavra secreta
+    secretword.disabled = 1; //Bloqueia o botão para que uma nova partida seja iniciada
+    yesButton.disabled = 1; 
+    noButton.disabled = 1; 
+    maybeButton.disabled = 1; 
+    correctButton.disabled = 1; 
+    abortButton.disabled = 1; 
+    ShowResult("ACERTOU",secretword.value,countyes,countno,countmaybe); //Insere o resumo do resultado
+    
 });
 
-correctButton.addEventListener('click',()=>{
+abortButton.addEventListener('click',()=> {
     secretword.type = "text";
+    secretword.disabled = 1;
+    yesButton.disabled = 1;
+    noButton.disabled = 1;
+    maybeButton.disabled = 1;
+    correctButton.disabled = 1;
+    abortButton.disabled = 1;
+    ShowResult("DESISTIU",secretword.value,countyes,countno,countmaybe);
 });
+
+restartButton.addEventListener('click',()=> {
+    countyes = 0; //reseta a contagem de acertos
+    countno = 0;
+    countmaybe = 0;
+    updatemaybeValue(); //atualiza na tela os acertos
+    updatenoValue();
+    updateyesValue();
+    secretword.type = "password"; //esconde a palavra secreta
+    secretword.value = ""; //limpa a palavra secreta
+    secretword.disabled = 0; //libera os botões
+    yesButton.disabled = 0;
+    noButton.disabled = 0;
+    maybeButton.disabled = 0;
+    correctButton.disabled = 0;
+    abortButton.disabled = 0;
+});
+
+
+function ShowResult(text0,text1,text2,text3,text4) {
+    document.getElementById("result").innerHTML +=
+     text0+ "<br>" 
+     +"- Palavra: " +"<strong>"+ text1 +"</strong>"+ "<br>"
+     +"- SIM: " +"<strong>"+ text2 +"</strong>"+ "<br>"
+     +"- NÃO: " +"<strong>"+ text3 +"</strong>"+ "<br>"
+     +"- TALVEZ: " +"<strong>"+ text4 +"</strong>"+ "<br>"
+     +"Tentativas Totais: " + "<strong>"+(text2+text3+text4)+"</strong>"+"<br><br>";
+  }
